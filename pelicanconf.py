a6c79cff89ffb00c88734d74d36275c8656664f2
Charith Ellawala <charith@lucidelectricdreams.com>
2016-05-18 21:04:41 +0100

    Update site config

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,12 +30,63 @@ AUTHOR_FEED_RSS = None
 # Social widget
 #SOCIAL = (('You can add links in your config file', '#'),
 #          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 NEST_HEADER_LOGO = '/images/logo.png'
+# Minified CSS
+NEST_CSS_MINIFY = True
+# Add items to top menu before pages
+MENUITEMS = [('Homepage', '/'),('Categories','/categories.html')]
+NEST_SITEMAP_COLUMN_TITLE = u'Sitemap'
+NEST_SITEMAP_MENU = [('Archives', '/archives.html'),('Tags','/tags.html')]
+NEST_SITEMAP_ATOM_LINK = u'Atom Feed'
+NEST_SITEMAP_RSS_LINK = u'RSS Feed'
+NEST_COPYRIGHT = u'&copy; 2016  Charith Ellawala '
+# index.html
+# index.html
+NEST_INDEX_HEAD_TITLE = u'Homepage'
+NEST_INDEX_CONTENT_TITLE = u'Recent Posts'
+NEST_INDEX_HEAD_TITLE = u'Homepage'
+NEST_INDEX_HEADER_TITLE = u'CEll\'s brain dumps' 
+
+NEST_ARCHIVES_HEAD_TITLE = u'Archives'
+NEST_ARCHIVES_HEAD_DESCRIPTION = u'Posts Archives'
+NEST_ARCHIVES_HEADER_TITLE = u'Archives'
+NEST_ARCHIVES_HEADER_SUBTITLE = u'Archives for all posts'
+NEST_ARCHIVES_CONTENT_TITLE = u'Archives'
+
+NEST_CATEGORIES_HEAD_TITLE = u'Categories'
+NEST_CATEGORIES_HEAD_DESCRIPTION = u'Archives listed by category'
+NEST_CATEGORIES_HEADER_TITLE = u'Categories'
+NEST_CATEGORIES_HEADER_SUBTITLE = u'Archives listed by category'
+
+NEST_CATEGORY_HEAD_TITLE = u'Category Archive'
+NEST_CATEGORY_HEAD_DESCRIPTION = u'Category Archive'
+NEST_CATEGORY_HEADER_TITLE = u'Category'
+NEST_CATEGORY_HEADER_SUBTITLE = u'Category Archive'
+# pagination.html
+NEST_PAGINATION_PREVIOUS = u'Previous'
+NEST_PAGINATION_NEXT = u'Next'
+# period_archives.html
+NEST_PERIOD_ARCHIVES_HEAD_TITLE = u'Archives for'
+NEST_PERIOD_ARCHIVES_HEAD_DESCRIPTION = u'Archives for'
+NEST_PERIOD_ARCHIVES_HEADER_TITLE = u'Archives'
+NEST_PERIOD_ARCHIVES_HEADER_SUBTITLE = u'Archives for'
+NEST_PERIOD_ARCHIVES_CONTENT_TITLE = u'Archives for'
+# tag.html
+NEST_TAG_HEAD_TITLE = u'Tag archives'
+NEST_TAG_HEAD_DESCRIPTION = u'Tag archives'
+NEST_TAG_HEADER_TITLE = u'Tag'
+NEST_TAG_HEADER_SUBTITLE = u'Tag archives'
+# tags.html
+NEST_TAGS_HEAD_TITLE = u'Tags'
+NEST_TAGS_HEAD_DESCRIPTION = u'Tags List'
+NEST_TAGS_HEADER_TITLE = u'Tags'
+NEST_TAGS_HEADER_SUBTITLE = u'Tags List'
+NEST_TAGS_CONTENT_TITLE = u'Tags List'
+NEST_TAGS_CONTENT_LIST = u'tagged'
 
 # Static files
 STATIC_PATHS = ['images']
